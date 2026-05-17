8323b52ce33aef6149eae54719ca5e28c26a3c71
이건모 <gunmo1103@naver.com>
2026-05-17 19:54:48 +0900

    feat: 공통 응답 DTO / 예외처리 / 인증 기반 게시글 CRUD 리팩토링

--- src/main/java/ssafy/study/ssafystudy/post/controller/dto/PostRequest.java
+++ src/main/java/ssafy/study/ssafystudy/post/controller/dto/PostRequest.java
@@ -1,14 +1,18 @@
 package ssafy.study.ssafystudy.post.controller.dto;
 
+import ssafy.study.ssafystudy.member.entity.MemberEntity;
+import ssafy.study.ssafystudy.post.entity.PostEntity;
+
 public class PostRequest {
     private String title;
     private String content;
-    private String author;
 
     public PostRequest(String title, String content, String author) {
         this.title = title;
         this.content = content;
-        this.author = author;
+    }
+    public PostEntity toEntity(MemberEntity author) {
+        return PostEntity.create(title, content, author);
     }
 
     public String getTitle() {
@@ -17,9 +21,5 @@ public class PostRequest {
 
     public String getContent() {
         return content;
-    }
-
-    public String getAuthor() {
-        return author;
     }
 }
